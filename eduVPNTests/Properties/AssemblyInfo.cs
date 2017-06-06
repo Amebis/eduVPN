509fe4aa27cf56b10f47b48fdd3c6776acaaf7a5
Simon Rozman <simon@rozman.si>
2017-06-06 08:49:14 +0200

    Versioning updated

--- eduVPNTests/Properties/AssemblyInfo.cs
+++ eduVPNTests/Properties/AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Amebis")]
-[assembly: AssemblyProduct("eduVPNTests")]
+[assembly: AssemblyProduct("eduVPN")]
 [assembly: AssemblyCopyright("Copyright © Amebis 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -39,5 +39,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyInformationalVersion("1.0-alpha")]
