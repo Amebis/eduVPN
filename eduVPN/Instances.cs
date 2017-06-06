3c212debec611451c333376dcfbb63a02ce7e8ca
Simon Rozman <simon@rozman.si>
2017-06-06 11:39:07 +0200

    eduVPN.Instances class introduced

---
+++ eduVPN/Instances.cs
@@ -0,0 +1,132 @@
+﻿/*
+    Copyright 2017 Amebis
+
+    This file is part of eduVPN.
+
+    eduVPN is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    eduVPN is distributed in the hope that it will be useful, but
+    WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with eduVPN. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+
+namespace eduVPN
+{
+    /// <summary>
+    /// An eduVPN list of instances = VPN service providers
+    /// </summary>
+    public class Instances : List<Instance>
+    {
+        public Instances(Uri uri, byte[] pub_key = null)
+        {
+            // Load instances data.
+            var client = new WebClient();
+            string data = client.DownloadString(uri);
+
+            if (pub_key != null)
+            {
+                // Generate signature URI.
+                var builder_sig = new UriBuilder(uri);
+                builder_sig.Path += ".sig";
+                try
+                {
+                    // Load signature.
+                    byte[] signature = Convert.FromBase64String(client.DownloadString(builder_sig.Uri));
+
+                }
+                catch { }
+            }
+
+            // Parse data.
+            var obj = (Dictionary<string, object>)eduJSON.Parser.Parse(data);
+
+            // Parse all instances listed.
+            object instances;
+            if (obj.TryGetValue("instances", out instances) && instances.GetType() == typeof(List<object>))
+            {
+                foreach (var el in (List<object>)instances)
+                    if (el.GetType() == typeof(Dictionary<string, object>))
+                        Add(new Instance((Dictionary<string, object>)el));
+            }
+
+            // Parse sequence.
+            object seq;
+            Sequence = obj.TryGetValue("seq", out seq) && seq.GetType() == typeof(int) ? (uint)(int)seq : 0;
+
+            // Parse authorization data.
+            object authorization_type;
+            if (obj.TryGetValue("authorization_type", out authorization_type) && authorization_type.GetType() == typeof(string))
+            {
+                switch (((string)authorization_type).ToLower())
+                {
+                    case "federated": AuthType = AuthorizationType.Federated; break;
+                    case "distributed": AuthType = AuthorizationType.Distributed; break;
+                    default: AuthType = AuthorizationType.Local; break; // Assume local authorization type on all other values.
+                }
+            }
+            else
+                AuthType = AuthorizationType.Local;
+
+            SignedAt = null;
+            if (pub_key != null)
+            {
+                // Parse signed date.
+                object signed_at;
+                if (obj.TryGetValue("signed_at", out signed_at) && signed_at.GetType() == typeof(string))
+                {
+                    DateTime result;
+                    if (DateTime.TryParse((string)signed_at, out result))
+                        SignedAt = result;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Authorization type
+        /// </summary>
+        public enum AuthorizationType
+        {
+            /// <summary>
+            /// Access token is specific to each instance and cannot be used by other instances.
+            /// </summary>
+            Local,
+
+            /// <summary>
+            /// Access token is issued by a central OAuth server; all instances accept this token.
+            /// </summary>
+            Federated,
+
+            /// <summary>
+            /// Access token from any instance can be used by any other instance.
+            /// </summary>
+            Distributed
+        }
+
+        /// <summary>
+        /// Authorization type
+        /// </summary>
+        public AuthorizationType AuthType { get; }
+
+        /// <summary>
+        /// Version sequence
+        /// </summary>
+        public uint Sequence { get; }
+
+        /// <summary>
+        /// Signature timestamp
+        /// </summary>
+        public DateTime? SignedAt { get; }
+    }
+}
---
+++ eduVPNTests/InstancesTests.cs
@@ -0,0 +1,30 @@
+﻿/*
+    eduVPN - End-user friendly VPN
+
+    Copyright: 2017, The Commons Conservancy eduVPN Programme
+    SPDX-License-Identifier: GPL-3.0+
+*/
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Net;
+
+namespace eduVPN.Tests
+{
+    [TestClass()]
+    public class InstancesTests
+    {
+        [TestMethod()]
+        public void InstancesTest()
+        {
+            Instances insts;
+
+            // .NET 3.5 allows Schannel to use SSL 3 and TLS 1.0 by default. Instead of hacking user computer's registry, extend it in runtime.
+            // System.Net.SecurityProtocolType lacks appropriate constants prior to .NET 4.5.
+            ServicePointManager.SecurityProtocol = (SecurityProtocolType)0x0C00;
+
+            insts = new Instances(new Uri("https://static.eduvpn.nl/instances.json"));
+
+        }
+    }
+}
