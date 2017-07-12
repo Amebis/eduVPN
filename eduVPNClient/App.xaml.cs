fac3ff3b150076a86f0944eb8e4e6792a5de4cdb
Simon Rozman <simon@rozman.si>
2017-07-12 12:13:25 +0200

    Support for application registered URI Scheme added

--- eduVPNClient/App.xaml.cs
+++ eduVPNClient/App.xaml.cs
@@ -5,15 +5,18 @@
     SPDX-License-Identifier: GPL-3.0+
 */
 
+using Microsoft.Shell;
 using System.Net;
 using System.Windows;
+using System;
+using System.Collections.Generic;
 
 namespace eduVPNClient
 {
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
-    public partial class App : Application
+    public partial class App : Application, ISingleInstanceApp
     {
         #region Constructors
 
@@ -25,6 +28,42 @@ namespace eduVPNClient
             // .NET 3.5 allows Schannel to use SSL 3 and TLS 1.0 by default. Instead of hacking user computer's registry, extend it in runtime.
             // System.Net.SecurityProtocolType lacks appropriate constants prior to .NET 4.5.
             ServicePointManager.SecurityProtocol = (SecurityProtocolType)0x0C00;
+        }
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// Main application method
+        /// </summary>
+        [STAThread]
+        public static void Main()
+        {
+            if (SingleInstance<App>.InitializeAsFirstInstance("org.eduvpn.app"))
+            {
+                // First instance
+                var application = new App();
+                application.InitializeComponent();
+                application.Run();
+
+                // Allow single instance code to perform cleanup operations.
+                SingleInstance<App>.Cleanup();
+            }
+        }
+
+        #endregion
+
+        #region ISingleInstanceApp Implementation
+
+        /// <summary>
+        /// Handles secondary application instance invocation.
+        /// </summary>
+        /// <param name="args">Command line parameters</param>
+        /// <returns><c>true</c></returns>
+        public bool SignalExternalCommandLineArgs(IList<string> args)
+        {
+            return true;
         }
 
         #endregion
