3478b27160e924a028a84b43e9fff44acd941743
Simon Rozman <simon@rozman.si>
2017-07-19 18:23:02 +0200

    Profile list simplified

--- eduVPN/ProfileList.cs
+++ eduVPN/ProfileList.cs
@@ -7,8 +7,6 @@
 
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.ComponentModel;
-using System.Threading;
 
 namespace eduVPN
 {
@@ -17,34 +15,18 @@ namespace eduVPN
     /// </summary>
     public class ProfileList : ObservableCollection<Profile>
     {
-        #region Properties
-
-        /// <summary>
-        /// Is the profile list OK?
-        /// </summary>
-        public bool IsOK
-        {
-            get { return _is_ok; }
-            set { if (value != _is_ok) { _is_ok = value; OnPropertyChanged(new PropertyChangedEventArgs("IsOK")); } }
-        }
-        private bool _is_ok;
-
-        #endregion
-
         #region Methods
 
         /// <summary>
         /// Loads profile list from a dictionary object (provided by JSON)
         /// </summary>
-        /// <param name="obj">Key/value dictionary with <c>profile_list</c> and other optional elements.</param>
-        public void Load(Dictionary<string, object> obj)
+        /// <param name="obj">List of key/value dictionaries with <c>profile_list</c> and other optional elements.</param>
+        public void Load(List<object> obj)
         {
             Clear();
 
-            var profile_list = eduJSON.Parser.GetValue<Dictionary<string, object>>(obj, "profile_list");
-
             // Parse all profiles listed. Don't do it in parallel to preserve the sort order.
-            foreach (var el in eduJSON.Parser.GetValue<List<object>>(profile_list, "data"))
+            foreach (var el in obj)
             {
                 if (el.GetType() == typeof(Dictionary<string, object>))
                 {
@@ -53,19 +35,6 @@ namespace eduVPN
                     Add(profile);
                 }
             }
-
-            // Parse OK flag.
-            IsOK = eduJSON.Parser.GetValue<bool>(profile_list, "ok", out bool ok) ? ok : true;
-        }
-
-        /// <summary>
-        /// Loads profile list from a JSON string
-        /// </summary>
-        /// <param name="json">JSON string</param>
-        /// <param name="ct">The token to monitor for cancellation requests.</param>
-        public void Load(string json, CancellationToken ct = default(CancellationToken))
-        {
-            Load((Dictionary<string, object>)eduJSON.Parser.Parse(json, ct));
         }
 
         #endregion
