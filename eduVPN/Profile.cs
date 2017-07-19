5a17b0b214a7a9b0a05293da8a28cee79ceefac0
Simon Rozman <simon@rozman.si>
2017-07-19 12:59:42 +0200

    VPN profile and list of profiles models introduced

---
+++ eduVPN/Profile.cs
@@ -0,0 +1,81 @@
+﻿/*
+    eduVPN - End-user friendly VPN
+
+    Copyright: 2017, The Commons Conservancy eduVPN Programme
+    SPDX-License-Identifier: GPL-3.0+
+*/
+
+using Prism.Mvvm;
+using System.Collections.Generic;
+
+namespace eduVPN
+{
+    /// <summary>
+    /// An eduVPN profile
+    /// </summary>
+    public class Profile : BindableBase
+    {
+        #region Properties
+
+        /// <summary>
+        /// Profile ID
+        /// </summary>
+        public string ID
+        {
+            get { return _id; }
+            set { if (value != _id) { _id = value; RaisePropertyChanged(); } }
+        }
+        private string _id;
+
+        /// <summary>
+        /// Profile name to display in GUI
+        /// </summary>
+        public string DisplayName
+        {
+            get { return _display_name; }
+            set { if (value != _display_name) { _display_name = value; RaisePropertyChanged(); } }
+        }
+        private string _display_name;
+
+        /// <summary>
+        /// Is two-factor authentication enabled for this profile?
+        /// </summary>
+        public bool IsTwoFactorAuthentication
+        {
+            get { return _is_two_factor_authentication;  }
+            set { if (value != _is_two_factor_authentication) { _is_two_factor_authentication = value; RaisePropertyChanged(); } }
+        }
+        private bool _is_two_factor_authentication;
+
+        #endregion
+
+        #region Methods
+
+        public override string ToString()
+        {
+            return DisplayName;
+        }
+
+        /// <summary>
+        /// Loads profile from a dictionary object (provided by JSON)
+        /// </summary>
+        /// <param name="obj">Key/value dictionary with <c>display_name</c>, <c>profile_id</c> and <c>two_factor</c> elements. <c>profile_id</c> is required. <c>display_name</c> and <c>profile_id</c> elements should be strings; <c>two_factor</c> should be boolean.</param>
+        public void Load(Dictionary<string, object> obj)
+        {
+            // Set ID.
+            ID = eduJSON.Parser.GetValue<string>(obj, "profile_id");
+
+            // Set display name.
+            if (eduJSON.Parser.GetValue(obj, "display_name", out string display_name))
+                DisplayName = display_name;
+            else
+                DisplayName = ID;
+
+            // Set two-factor authentication.
+            if (eduJSON.Parser.GetValue(obj, "two_factor", out bool two_factor))
+                IsTwoFactorAuthentication = two_factor;
+        }
+
+        #endregion
+    }
+}
---
+++ eduVPN/ProfileList.cs
@@ -0,0 +1,73 @@
+﻿/*
+    eduVPN - End-user friendly VPN
+
+    Copyright: 2017, The Commons Conservancy eduVPN Programme
+    SPDX-License-Identifier: GPL-3.0+
+*/
+
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Threading;
+
+namespace eduVPN
+{
+    /// <summary>
+    /// An eduVPN list of profiles
+    /// </summary>
+    public class ProfileList : ObservableCollection<Profile>
+    {
+        #region Properties
+
+        /// <summary>
+        /// Is the profile list OK?
+        /// </summary>
+        public bool IsOK
+        {
+            get { return _is_ok; }
+            set { if (value != _is_ok) { _is_ok = value; OnPropertyChanged(new PropertyChangedEventArgs("IsOK")); } }
+        }
+        private bool _is_ok;
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// Loads profile list from a dictionary object (provided by JSON)
+        /// </summary>
+        /// <param name="obj">Key/value dictionary with <c>profile_list</c> and other optional elements.</param>
+        public void Load(Dictionary<string, object> obj)
+        {
+            Clear();
+
+            var profile_list = eduJSON.Parser.GetValue<Dictionary<string, object>>(obj, "profile_list");
+
+            // Parse all profiles listed. Don't do it in parallel to preserve the sort order.
+            foreach (var el in eduJSON.Parser.GetValue<List<object>>(profile_list, "data"))
+            {
+                if (el.GetType() == typeof(Dictionary<string, object>))
+                {
+                    var profile = new Profile();
+                    profile.Load((Dictionary<string, object>)el);
+                    Add(profile);
+                }
+            }
+
+            // Parse OK flag.
+            IsOK = eduJSON.Parser.GetValue<bool>(profile_list, "ok", out bool ok) ? ok : true;
+        }
+
+        /// <summary>
+        /// Loads profile list from a JSON string
+        /// </summary>
+        /// <param name="json">JSON string</param>
+        /// <param name="ct">The token to monitor for cancellation requests.</param>
+        public void Load(string json, CancellationToken ct = default(CancellationToken))
+        {
+            Load((Dictionary<string, object>)eduJSON.Parser.Parse(json, ct));
+        }
+
+        #endregion
+    }
+}
