da22ea61d26fdb0f118b1c5ba42674e0eeb1d2fb
Simon Rozman <simon@rozman.si>
2017-07-20 11:33:57 +0200

    LoadJSONAPIResponse extension introduced

---
+++ eduVPN/JSON/APIErrorException.cs
@@ -0,0 +1,43 @@
+﻿/*
+    eduVPN - End-user friendly VPN
+
+    Copyright: 2017, The Commons Conservancy eduVPN Programme
+    SPDX-License-Identifier: GPL-3.0+
+*/
+
+using System;
+using System.Runtime.Serialization;
+using System.Security.Permissions;
+
+namespace eduVPN.JSON
+{
+    /// <summary>
+    /// API server replied with an error.
+    /// </summary>
+    [Serializable]
+    public class APIErrorException : ApplicationException
+    {
+        #region Constructors
+
+        /// <summary>
+        /// Constructs an exception
+        /// </summary>
+        /// <param name="code">JSON code</param>
+        /// <param name="start">Starting offset in <paramref name="code"/>.</param>
+        public APIErrorException() :
+            this(Resources.Strings.ErrorAPIServerReply)
+        {
+        }
+
+        /// <summary>
+        /// Constructs an exception
+        /// </summary>
+        /// <param name="message">Exception message</param>
+        public APIErrorException(string message) :
+            base(message)
+        {
+        }
+
+        #endregion
+    }
+}
--- eduVPN/JSON/Extensions.cs
+++ eduVPN/JSON/Extensions.cs
@@ -5,6 +5,7 @@
     SPDX-License-Identifier: GPL-3.0+
 */
 
+using System.Collections.Generic;
 using System.Threading;
 
 namespace eduVPN.JSON
@@ -20,6 +21,28 @@ namespace eduVPN.JSON
         public static void LoadJSON(this ILoadableItem i, string json, CancellationToken ct = default(CancellationToken))
         {
             i.Load(eduJSON.Parser.Parse(json, ct));
+        }
+
+        /// <summary>
+        /// Loads class from a JSON string provided by API
+        /// </summary>
+        /// <param name="i">Loadable item</param>
+        /// <param name="json">JSON string representing a dictionary of key/values with <paramref name="name"/> element.</param>
+        /// <param name="name">The name of the value holder element</param>
+        /// <param name="ct">The token to monitor for cancellation requests.</param>
+        public static void LoadJSONAPIResponse(this ILoadableItem i, string json, string name, CancellationToken ct = default(CancellationToken))
+        {
+            // Parse JSON string and get inner key/value dictionary.
+            var obj = eduJSON.Parser.GetValue<Dictionary<string, object>>(
+                (Dictionary<string, object>)eduJSON.Parser.Parse(json, ct),
+                name);
+
+            // Verify response status.
+            if (eduJSON.Parser.GetValue(obj, "ok", out bool is_ok) && !is_ok)
+                throw new APIErrorException();
+
+            // Load data.
+            i.Load(obj["data"]);
         }
     }
 }
