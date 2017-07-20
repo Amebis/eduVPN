e6a3f20aa053f33048ca92dfa386699499202802
Simon Rozman <simon@rozman.si>
2017-07-20 09:18:46 +0200

    Profile selection wizard page introduced

---
+++ eduVPN/ViewModels/ProfileSelectViewModel.cs
@@ -0,0 +1,28 @@
+﻿/*
+    eduVPN - End-user friendly VPN
+
+    Copyright: 2017, The Commons Conservancy eduVPN Programme
+    SPDX-License-Identifier: GPL-3.0+
+*/
+
+namespace eduVPN.ViewModels
+{
+    /// <summary>
+    /// Profile selection wizard page
+    /// </summary>
+    class ProfileSelectViewModel : ConnectWizardPageViewModel
+    {
+        #region Constructors
+
+        /// <summary>
+        /// Constructs an profile selection wizard page.
+        /// </summary>
+        /// <param name="parent">The page parent</param>
+        public ProfileSelectViewModel(ConnectWizardViewModel parent) :
+            base(parent)
+        {
+        }
+
+        #endregion
+    }
+}
