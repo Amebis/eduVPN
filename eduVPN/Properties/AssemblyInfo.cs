ce0de901a8cdc70d509b3e88ad64074576e05245
Simon Rozman <simon@rozman.si>
2017-08-28 23:22:17 +0200

    Version set to 1.0-alpha1

--- eduVPN/Properties/AssemblyInfo.cs
+++ eduVPN/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 //      Minor Version
 //      Build Number
 //      Revision
-[assembly: AssemblyVersion("1.0.0.*")]
-[assembly: AssemblyInformationalVersion("1.0-alpha")]
+[assembly: AssemblyVersion("1.0.1.*")]
+[assembly: AssemblyInformationalVersion("1.0-alpha1")]
