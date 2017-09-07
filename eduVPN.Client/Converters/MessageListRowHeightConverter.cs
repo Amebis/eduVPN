aab626ee0cea236ad4e274f5b6f411f1a335db47
Simon Rozman <simon@rozman.si>
2017-09-07 15:18:31 +0200

    When no messages available the message area is collapsed

---
+++ eduVPN.Client/Converters/MessageListRowHeightConverter.cs
@@ -0,0 +1,30 @@
+﻿/*
+    eduVPN - End-user friendly VPN
+
+    Copyright: 2017, The Commons Conservancy eduVPN Programme
+    SPDX-License-Identifier: GPL-3.0+
+*/
+
+using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace eduVPN.Client.Converters
+{
+    /// <summary>
+    /// Returns <c>1*</c> if message list contains at least one message; or <c>Auto</c> otherwise.
+    /// </summary>
+    public class MessageListRowHeightConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value is int count && count > 0 ? new GridLength(1.0, GridUnitType.Star) : GridLength.Auto;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
