5af995e3d3efb95b092d9359692da5fc8f749be9
Simon Rozman <simon@rozman.si>
2017-09-18 13:37:28 +0200

    RequestInstanceAuthorizationEventArgs simplified

--- eduVPN/ViewModels/RequestInstanceAuthorizationEventArgs.cs
+++ eduVPN/ViewModels/RequestInstanceAuthorizationEventArgs.cs
@@ -5,15 +5,12 @@
     SPDX-License-Identifier: GPL-3.0+
 */
 
-using eduOAuth;
-using System;
-
 namespace eduVPN.ViewModels
 {
     /// <summary>
     /// RequestInstanceAuthorization event arguments
     /// </summary>
-    public class RequestInstanceAuthorizationEventArgs : EventArgs
+    public class RequestInstanceAuthorizationEventArgs : Models.RequestAuthorizationEventArgs
     {
         #region Properties
 
@@ -21,12 +18,6 @@ namespace eduVPN.ViewModels
         /// Authenticating instance
         /// </summary>
         public Models.InstanceInfo Instance { get; }
-
-        /// <summary>
-        /// Access token
-        /// </summary>
-        /// <remarks>Should be populated by access token on event end, or <c>null</c> if authorization failed.</remarks>
-        public AccessToken AccessToken { get; set; }
 
         #endregion
 
