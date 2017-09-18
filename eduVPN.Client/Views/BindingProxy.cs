f1cab68d1da1e9a778338ebc41d32688def78a46
Simon Rozman <simon@rozman.si>
2017-09-18 18:40:42 +0200

    Pop-up menu to forget stale configurations introduced (addresses #20)

---
+++ eduVPN.Client/Views/BindingProxy.cs
@@ -0,0 +1,40 @@
+﻿/*
+    eduVPN - End-user friendly VPN
+
+    Copyright: 2017, The Commons Conservancy eduVPN Programme
+    SPDX-License-Identifier: GPL-3.0+
+*/
+
+using System.Windows;
+
+namespace eduVPN.Views
+{
+    /// <summary>
+    /// Helper class to save the binding context for later reuse
+    /// </summary>
+    public class BindingProxy : Freezable
+    {
+        #region Properties
+
+        /// <summary>
+        /// Binding data
+        /// </summary>
+        public object Data
+        {
+            get { return GetValue(DataProperty); }
+            set { SetValue(DataProperty, value); }
+        }
+        public static readonly DependencyProperty DataProperty = DependencyProperty.Register("Data", typeof(object), typeof(BindingProxy), null);
+
+        #endregion
+
+        #region Methods
+
+        protected override Freezable CreateInstanceCore()
+        {
+            return new BindingProxy();
+        }
+
+        #endregion
+    }
+}
