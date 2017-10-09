Amebis/eduVPN

commit 54cc4fd06db84ae0a35a10e68fb39862c755102c

Access token handling redesigned to avoid racing condition, and to support access token reuse for clones of the same instance (closes #25)

eduVPN/ViewModels/AuthenticatingInstanceSelectPage.cs
@@ -46,7 +46,7 @@ namespace eduVPN.ViewModels
                             try
                             {
                                 // Trigger initial authorization request.
-                                var authorization_task = new Task(() => SelectedInstance.GetAccessToken(Window.Abort.Token), Window.Abort.Token, TaskCreationOptions.LongRunning);
+                                var authorization_task = new Task(() => Parent.Instance_RequestAuthorization(SelectedInstance, new Models.RequestAuthorizationEventArgs("config")), Window.Abort.Token, TaskCreationOptions.LongRunning);
                                 authorization_task.Start();
                                 await authorization_task;
 

eduVPN/ViewModels/ConfigurationSelectPanel.cs
@@ -48,7 +48,7 @@ namespace eduVPN.ViewModels
                             try
                             {
                                 // Trigger initial authorization request.
-                                var authorization_task = new Task(() => SelectedConfiguration.AuthenticatingInstance.GetAccessToken(Window.Abort.Token), Window.Abort.Token, TaskCreationOptions.LongRunning);
+                                var authorization_task = new Task(() => Parent.Instance_RequestAuthorization(SelectedConfiguration, new Models.RequestAuthorizationEventArgs("config")), Window.Abort.Token, TaskCreationOptions.LongRunning);
                                 authorization_task.Start();
                                 await authorization_task;
 

eduVPN/ViewModels/ConnectWizard.cs
@@ -5,6 +5,7 @@
     SPDX-License-Identifier: GPL-3.0+
 */
 
+using eduOAuth;
 using Prism.Commands;
 using System;
 using System.Collections.Generic;
@@ -39,6 +40,11 @@ namespace eduVPN.ViewModels
         /// The alpha factor to increase/decrease popularity
         /// </summary>
         private static readonly float _popularity_alpha = 0.1f;
+
+        /// <summary>
+        /// Access token cache serialization
+        /// </summary>
+        private static object _access_token_cache_lock = new object();
 
         #endregion
 
@@ -738,7 +744,6 @@ namespace eduVPN.ViewModels
                                                 // Matching profile with existing profiles might trigger OAuth in GetProfileList().
                                                 // Unfortunately, we can't rely to stored version from settings, since available profiles and their 2FA settings might have changed.
                                                 cfg.ConnectingProfile = cfg.ConnectingInstance.GetProfileList(cfg.AuthenticatingInstance, Abort.Token).Where(p => p.ID == h_local.Profile.ID).FirstOrDefault();
-                                                //cfg.ConnectingProfile = h_local.Profile;
                                                 if (cfg.ConnectingProfile == null) return;
                                             }
                                             else if (_instance_sources[source_index] is Models.DistributedInstanceSourceInfo instance_source_distributed &&
@@ -838,25 +843,71 @@ namespace eduVPN.ViewModels
         /// <param name="e">Authorization request event arguments</param>
         public void Instance_RequestAuthorization(object sender, Models.RequestAuthorizationEventArgs e)
         {
-            // Re-raise this event as ConnectWizard event, to simplify view.
-            // This way the view can listen ConnectWizard for authentication events only.
-            if (Dispatcher.CurrentDispatcher == Dispatcher)
+            if (sender is Models.InstanceInfo authenticating_instance)
+            lock (_access_token_cache_lock)
             {
-                // We're in the GUI thread.
-                var e_instance = new RequestInstanceAuthorizationEventArgs((Models.InstanceInfo)sender);
-                RequestInstanceAuthorization?.Invoke(this, e_instance);
-                e.AccessToken = e_instance.AccessToken;
-            }
-            else
-            {
-                // We're in the background thread - raise event via dispatcher.
-                Dispatcher.Invoke(DispatcherPriority.Normal,
-                    (Action)(() =>
+                // Get API endpoints.
+                var api = authenticating_instance.GetEndpoints(Abort.Token);
+
+                if (e.SourcePolicy != Models.RequestAuthorizationEventArgs.SourcePolicyType.ForceAuthorization)
+                {
+                    try
                     {
-                        var e_instance = new RequestInstanceAuthorizationEventArgs((Models.InstanceInfo)sender);
+                        // Try to load the access token from the settings.
+                        var access_token = AccessToken.FromBase64String(Properties.Settings.Default.AccessTokens[api.AuthorizationEndpoint.AbsoluteUri]);
+                        if (access_token.Expires.HasValue && access_token.Expires.Value <= DateTime.Now)
+                        {
+                            // Token expired. Refresh it.
+                            access_token = access_token.RefreshToken(api.TokenEndpoint, null, Abort.Token);
+                            if (access_token != null)
+                            {
+                                // Update access token in the settings.
+                                Properties.Settings.Default.AccessTokens[api.AuthorizationEndpoint.AbsoluteUri] = access_token.ToBase64String();
+
+                                // If we got here, return the token.
+                                e.AccessToken = access_token;
+                                return;
+                            }
+                        }
+                        else
+                        {
+                            // If we got here, return the token.
+                            e.AccessToken = access_token;
+                            return;
+                        }
+                    }
+                    catch { }
+                }
+
+                if (e.SourcePolicy != Models.RequestAuthorizationEventArgs.SourcePolicyType.SavedOnly)
+                {
+                    // Re-raise this event as ConnectWizard event, to simplify view.
+                    // This way the view can listen ConnectWizard for authentication events only.
+                    if (Dispatcher.CurrentDispatcher == Dispatcher)
+                    {
+                        // We're in the GUI thread.
+                        var e_instance = new RequestInstanceAuthorizationEventArgs((Models.InstanceInfo)sender, e.Scope);
                         RequestInstanceAuthorization?.Invoke(this, e_instance);
                         e.AccessToken = e_instance.AccessToken;
-                    }));
+                    }
+                    else
+                    {
+                        // We're in the background thread - raise event via dispatcher.
+                        Dispatcher.Invoke(DispatcherPriority.Normal,
+                            (Action)(() =>
+                            {
+                                var e_instance = new RequestInstanceAuthorizationEventArgs((Models.InstanceInfo)sender, e.Scope);
+                                RequestInstanceAuthorization?.Invoke(this, e_instance);
+                                e.AccessToken = e_instance.AccessToken;
+                            }));
+                    }
+
+                    if (e.AccessToken != null)
+                    {
+                        // Save access token to the settings.
+                        Properties.Settings.Default.AccessTokens[api.AuthorizationEndpoint.AbsoluteUri] = e.AccessToken.ToBase64String();
+                    }
+                }
             }
         }
 

eduVPN/ViewModels/CustomInstancePage.cs
@@ -52,7 +52,7 @@ namespace eduVPN.ViewModels
                                 selected_instance.RequestAuthorization += Parent.Instance_RequestAuthorization;
 
                                 // Trigger initial authorization request.
-                                var authorization_task = new Task(() => selected_instance.GetAccessToken(Window.Abort.Token), Window.Abort.Token, TaskCreationOptions.LongRunning);
+                                var authorization_task = new Task(() => Parent.Instance_RequestAuthorization(selected_instance, new Models.RequestAuthorizationEventArgs("config")), Window.Abort.Token, TaskCreationOptions.LongRunning);
                                 authorization_task.Start();
                                 await authorization_task;
 

eduVPN/ViewModels/InstanceSourceSelectPage.cs
@@ -45,7 +45,7 @@ namespace eduVPN.ViewModels
                                 }
                                 else if (Parent.InstanceSource is Models.DistributedInstanceSourceInfo instance_source_distributed)
                                 {
-                                    // Check if we have access token for any of the instances.
+                                    // Check if we have saved access token for any of the instances.
                                     object authenticating_instance_lock = new object();
                                     Models.InstanceInfo authenticating_instance = null;
                                     await Task.WhenAll(Parent.InstanceSource.Select(instance =>
@@ -53,7 +53,9 @@ namespace eduVPN.ViewModels
                                         var authorization_task = new Task(
                                             () =>
                                             {
-                                                if (instance.PeekAccessToken(Window.Abort.Token) != null)
+                                                var e = new Models.RequestAuthorizationEventArgs("config") { SourcePolicy = Models.RequestAuthorizationEventArgs.SourcePolicyType.SavedOnly };
+                                                Parent.Instance_RequestAuthorization(instance, e);
+                                                if (e.AccessToken != null)
                                                     lock (authenticating_instance_lock)
                                                         authenticating_instance = instance;
                                             },
@@ -81,7 +83,7 @@ namespace eduVPN.ViewModels
                                     authenticating_instance.RequestAuthorization += Parent.Instance_RequestAuthorization;
 
                                     // Trigger initial authorization request.
-                                    var authorization_task = new Task(() => authenticating_instance.GetAccessToken(Window.Abort.Token), Window.Abort.Token, TaskCreationOptions.LongRunning);
+                                    var authorization_task = new Task(() => Parent.Instance_RequestAuthorization(authenticating_instance, new Models.RequestAuthorizationEventArgs("config")), Window.Abort.Token, TaskCreationOptions.LongRunning);
                                     authorization_task.Start();
                                     await authorization_task;
 

eduVPN.Client/Views/AuthorizationPopup.xaml
@@ -37,6 +37,6 @@
             Margin="5"
             Style="{StaticResource eduVPNButtonStyle}"
             Content="{x:Static res:Strings.AuthorizationPopupRetry}"
-            Command="{Binding Path=RetryAuthorization}"/>
+            Command="{Binding Path=RequestAuthorization}"/>
     </Grid>
 </view:Window>

eduVPN.Client/Views/ConnectWizard.xaml.cs
@@ -192,8 +192,10 @@ namespace eduVPN.Views
 
             // Trigger authorization.
             var view_model = AuthorizationPopup.DataContext as ViewModels.AuthorizationPopup;
-            if (view_model.RequestAuthorization.CanExecute(e.Instance))
-                view_model.RequestAuthorization.Execute(e.Instance);
+            view_model.AuthenticatingInstance = e.Instance;
+            view_model.Scope = e.Scope;
+            if (view_model.RequestAuthorization.CanExecute())
+                view_model.RequestAuthorization.Execute();
 
             // Run the authorization pop-up and pass the access token to be returned to the event sender.
             if (AuthorizationPopup.ShowDialog() == true)

commit c5cec3ab0d126c728907a79dee57ccb54ebde79e

Permissive initial authorization issue introduced in 54cc4fd06db84ae0a35a10e68fb39862c755102c resolved

eduVPN/ViewModels/AuthenticatingInstanceSelectPage.cs
@@ -8,7 +8,6 @@
 using Prism.Commands;
 using System;
 using System.ComponentModel;
-using System.Threading.Tasks;
 
 namespace eduVPN.ViewModels
 {
@@ -46,9 +45,7 @@ namespace eduVPN.ViewModels
                             try
                             {
                                 // Trigger initial authorization request.
-                                var authorization_task = new Task(() => Parent.Instance_RequestAuthorization(SelectedInstance, new Models.RequestAuthorizationEventArgs("config")), Window.Abort.Token, TaskCreationOptions.LongRunning);
-                                authorization_task.Start();
-                                await authorization_task;
+                                await Parent.TriggerAuthorizationAsync(SelectedInstance);
 
                                 // Save selected instance.
                                 Parent.AuthenticatingInstance = SelectedInstance;

eduVPN/ViewModels/ConfigurationSelectPanel.cs
@@ -10,7 +10,6 @@ using System;
 using System.Collections.Specialized;
 using System.ComponentModel;
 using System.Linq;
-using System.Threading.Tasks;
 
 namespace eduVPN.ViewModels
 {
@@ -48,9 +47,7 @@ namespace eduVPN.ViewModels
                             try
                             {
                                 // Trigger initial authorization request.
-                                var authorization_task = new Task(() => Parent.Instance_RequestAuthorization(SelectedConfiguration, new Models.RequestAuthorizationEventArgs("config")), Window.Abort.Token, TaskCreationOptions.LongRunning);
-                                authorization_task.Start();
-                                await authorization_task;
+                                await Parent.TriggerAuthorizationAsync(SelectedConfiguration.AuthenticatingInstance);
 
                                 // Start VPN session.
                                 var param = new ConnectWizard.StartSessionParams(

eduVPN/ViewModels/ConnectWizard.cs
@@ -837,6 +837,23 @@ namespace eduVPN.ViewModels
         #region Methods
 
         /// <summary>
+        /// Triggers authorization for selected instance asynchronously
+        /// </summary>
+        /// <param name="instance">Instance</param>
+        /// <returns>Asynchronous operation</returns>
+        /// <exception cref="AccessTokenNullException">Authorization failed</exception>
+        public async Task TriggerAuthorizationAsync(Models.InstanceInfo instance)
+        {
+            var e = new Models.RequestAuthorizationEventArgs("config");
+            var authorization_task = new Task(() => Instance_RequestAuthorization(instance, e), Abort.Token, TaskCreationOptions.LongRunning);
+            authorization_task.Start();
+            await authorization_task;
+
+            if (e.AccessToken == null)
+                throw new Models.AccessTokenNullException();
+        }
+
+        /// <summary>
         /// Called when an instance requests user authorization
         /// </summary>
         /// <param name="sender">Instance of type <c>eduVPN.Models.InstanceInfo</c> requiring authorization</param>

eduVPN/ViewModels/CustomInstancePage.cs
@@ -9,7 +9,6 @@ using Prism.Commands;
 using System;
 using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
-using System.Threading.Tasks;
 
 namespace eduVPN.ViewModels
 {
@@ -52,9 +51,7 @@ namespace eduVPN.ViewModels
                                 selected_instance.RequestAuthorization += Parent.Instance_RequestAuthorization;
 
                                 // Trigger initial authorization request.
-                                var authorization_task = new Task(() => Parent.Instance_RequestAuthorization(selected_instance, new Models.RequestAuthorizationEventArgs("config")), Window.Abort.Token, TaskCreationOptions.LongRunning);
-                                authorization_task.Start();
-                                await authorization_task;
+                                await Parent.TriggerAuthorizationAsync(selected_instance);
 
                                 // Set authentication instance.
                                 Parent.AuthenticatingInstance = selected_instance;

eduVPN/ViewModels/InstanceSourceSelectPage.cs
@@ -83,9 +83,7 @@ namespace eduVPN.ViewModels
                                     authenticating_instance.RequestAuthorization += Parent.Instance_RequestAuthorization;
 
                                     // Trigger initial authorization request.
-                                    var authorization_task = new Task(() => Parent.Instance_RequestAuthorization(authenticating_instance, new Models.RequestAuthorizationEventArgs("config")), Window.Abort.Token, TaskCreationOptions.LongRunning);
-                                    authorization_task.Start();
-                                    await authorization_task;
+                                    await Parent.TriggerAuthorizationAsync(authenticating_instance);
 
                                     // Set authenticating instance.
                                     Parent.AuthenticatingInstance = authenticating_instance;