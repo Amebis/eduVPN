9aa56c233a4231e23e5fd1ccd367253a752c206f
Simon Rozman <simon@rozman.si>
2017-11-29 12:26:30 +0100

    Finish support for JSON loading of ObservableCollection(s)

--- eduVPN/System/Collections/ObjectModel/Extensions.cs
+++ eduVPN/System/Collections/ObjectModel/Extensions.cs
@@ -14,6 +14,31 @@ namespace System.Collections.ObjectModel
     public static class Extensions
     {
         /// <summary>
+        /// Loads class from a JSON string
+        /// </summary>
+        /// <param name="i">Loadable item</param>
+        /// <param name="json">JSON string</param>
+        /// <param name="ct">The token to monitor for cancellation requests</param>
+        public static void LoadJSON<T>(this ObservableCollection<T> i, string json, CancellationToken ct = default(CancellationToken)) where T : ILoadableItem, new()
+        {
+            var obj = eduJSON.Parser.Parse(json, ct);
+            if (obj is List<object> obj2)
+            {
+                i.Clear();
+
+                // Parse all items listed. Don't do it in parallel to preserve the sort order.
+                foreach (var el in obj2)
+                {
+                    var item = new T();
+                    item.Load(el);
+                    i.Add(item);
+                }
+            }
+            else
+                throw new eduJSON.InvalidParameterTypeException(nameof(obj), typeof(List<object>), obj.GetType());
+        }
+
+        /// <summary>
         /// Loads class from a JSON string provided by API
         /// </summary>
         /// <param name="i">Loadable item</param>
