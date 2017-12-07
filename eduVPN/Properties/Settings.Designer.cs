Amebis/eduVPN

commit 6d756c00c0e9d91abdee593c91ddd5062dbd6311

Change default mode to 3

eduVPN/Properties/Settings.Designer.cs
@@ -195,7 +195,7 @@ namespace eduVPN.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("0")]
+        [global::System.Configuration.DefaultSettingValueAttribute("3")]
         public int SettingsVersion {
             get {
                 return ((int)(this["SettingsVersion"]));

eduVPN/Properties/Settings.settings
@@ -51,7 +51,7 @@
       <Value Profile="(Default)">&lt;SerializableStringDictionary/&gt;</Value>
     </Setting>
     <Setting Name="SettingsVersion" Type="System.Int32" Scope="User">
-      <Value Profile="(Default)">0</Value>
+      <Value Profile="(Default)">3</Value>
     </Setting>
   </Settings>
 </SettingsFile>

eduVPN.Client/app.config
@@ -86,7 +86,7 @@
                 when the mode is changed.
             -->
             <setting name="ConnectingProfileSelectMode" serializeAs="String">
-                <value>0</value>
+                <value>3</value>
             </setting>
         </eduVPN.Properties.Settings>
     </applicationSettings>
@@ -109,13 +109,6 @@
             -->
             <setting name="OpenVPNForceTCP" serializeAs="String">
                 <value>False</value>
-            </setting>
-
-            <!--
-                Reserved for internal use
-            -->
-            <setting name="SettingsVersion" serializeAs="String">
-                <value>0</value>
             </setting>
         </eduVPN.Properties.Settings>
         <eduVPN.Client.Properties.Settings>

commit 0bbe53d8aa7aac099823f03fcf27bf55cae2f33a

Swap SettingsVersion and ConnectingProfileSelectMode
Fix the incorrect defaults caused by
6d756c00c0e9d91abdee593c91ddd5062dbd6311.

eduVPN/Properties/Settings.Designer.cs
@@ -150,7 +150,7 @@ namespace eduVPN.Properties {
         
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("0")]
+        [global::System.Configuration.DefaultSettingValueAttribute("3")]
         public int ConnectingProfileSelectMode {
             get {
                 return ((int)(this["ConnectingProfileSelectMode"]));
@@ -195,7 +195,7 @@ namespace eduVPN.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("3")]
+        [global::System.Configuration.DefaultSettingValueAttribute("0")]
         public int SettingsVersion {
             get {
                 return ((int)(this["SettingsVersion"]));

eduVPN/Properties/Settings.settings
@@ -39,7 +39,7 @@
       <Value Profile="(Default)">openvpn$eduVPN\service</Value>
     </Setting>
     <Setting Name="ConnectingProfileSelectMode" Type="System.Int32" Scope="Application">
-      <Value Profile="(Default)">0</Value>
+      <Value Profile="(Default)">3</Value>
     </Setting>
     <Setting Name="OpenVPNInterfaceID" Type="System.Guid" Scope="User">
       <Value Profile="(Default)">00000000-0000-0000-0000-000000000000</Value>
@@ -51,7 +51,7 @@
       <Value Profile="(Default)">&lt;SerializableStringDictionary/&gt;</Value>
     </Setting>
     <Setting Name="SettingsVersion" Type="System.Int32" Scope="User">
-      <Value Profile="(Default)">3</Value>
+      <Value Profile="(Default)">0</Value>
     </Setting>
   </Settings>
 </SettingsFile>