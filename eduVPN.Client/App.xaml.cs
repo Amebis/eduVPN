41de0bb38937d1a8d2df7b558af0406e917b3a30
Simon Rozman <simon@rozman.si>
2017-12-15 12:14:14 +0100

    Add support for alternative app.config file
    (closes #56)

--- eduVPN.Client/App.xaml.cs
+++ eduVPN.Client/App.xaml.cs
@@ -30,6 +30,29 @@ namespace eduVPN.Client
         [STAThread]
         public static void Main()
         {
+            // If app.local.config file is present, read configuration from it.
+            var app_config_file = AppDomain.CurrentDomain.SetupInformation.ConfigurationFile;
+            var local_config_file = Path.Combine(Path.GetDirectoryName(app_config_file), Path.GetFileNameWithoutExtension(app_config_file) + ".local.config");
+            if (File.Exists(local_config_file))
+            {
+                AppDomain.CurrentDomain.SetData("APP_CONFIG_FILE", local_config_file);
+
+                typeof(ConfigurationManager)
+                    .GetField("s_initState", BindingFlags.NonPublic | BindingFlags.Static)
+                    .SetValue(null, 0);
+
+                typeof(ConfigurationManager)
+                    .GetField("s_configSystem", BindingFlags.NonPublic | BindingFlags.Static)
+                    .SetValue(null, null);
+
+                typeof(ConfigurationManager)
+                    .Assembly.GetTypes()
+                    .Where(x => x.FullName == "System.Configuration.ClientConfigPaths")
+                    .First()
+                    .GetField("s_current", BindingFlags.NonPublic | BindingFlags.Static)
+                    .SetValue(null, null);
+            }
+
             if (!SingleInstance<App>.InitializeAsFirstInstance("org.eduvpn.app"))
             {
                 // This is not the first instance. Quit.
