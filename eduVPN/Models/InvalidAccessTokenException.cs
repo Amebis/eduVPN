55a1ab8dc2c60db244a0392bf3c68e4ca6eb8839
Simon Rozman <simon@rozman.si>
2019-11-20 14:32:34 +0100

    Cache failed authorization attempts
    When the client was started or tried to connect to an instance which
    access token expired, the authorization popup was displayed multiple
    times - for each action thread, if authorization failed and/or was
    canceled.
    
    Furthermore, this commit upgrades the error message displayed on failed
    authorization to make more sense to end-user unaware of OAuth tokens.
    
    Signed-off-by: Simon Rozman <simon@rozman.si>

---
+++ eduVPN/Models/InvalidAccessTokenException.cs
@@ -0,0 +1,56 @@
+﻿/*
+    eduVPN - VPN for education and research
+
+    Copyright: 2017-2019 The Commons Conservancy eduVPN Programme
+    SPDX-License-Identifier: GPL-3.0+
+*/
+
+using System;
+using System.Runtime.Serialization;
+
+namespace eduVPN.Models
+{
+    /// <summary>
+    /// No access token granted.
+    /// </summary>
+    [Serializable]
+    class InvalidAccessTokenException : Exception
+    {
+        #region Constructors
+
+        /// <summary>
+        /// Constructs an exception
+        /// </summary>
+        /// <param name="message">Exception message</param>
+        public InvalidAccessTokenException(string message) :
+            this(message, null)
+        {
+        }
+
+        /// <summary>
+        /// Constructs an exception
+        /// </summary>
+        /// <param name="message">Exception message</param>
+        /// <param name="innerException">Inner exception reference</param>
+        public InvalidAccessTokenException(string message, Exception innerException) :
+            base(message, innerException)
+        {
+        }
+
+        #endregion
+
+        #region ISerializable Support
+
+        /// <summary>
+        /// Deserialize object.
+        /// </summary>
+        /// <param name="info">The <see cref="SerializationInfo"/> populated with data.</param>
+        /// <param name="context">The source of this deserialization.</param>
+        protected InvalidAccessTokenException(SerializationInfo info, StreamingContext context)
+            : base(info, context)
+        {
+        }
+
+        #endregion
+    }
+}
