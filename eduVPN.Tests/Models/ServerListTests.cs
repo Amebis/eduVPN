b68e124bbac1a21779ca4abda7894e757dc24c9f
Simon Rozman <simon@rozman.si>
2020-12-19 15:27:19 +0100

    Enable TLS 1.3
    Signed-off-by: Simon Rozman <simon@rozman.si>

--- eduVPN.Tests/Models/ServerListTests.cs
+++ eduVPN.Tests/Models/ServerListTests.cs
@@ -55,7 +55,8 @@ namespace eduVPN.Models.Tests
         {
             // .NET 3.5 allows Schannel to use SSL 3 and TLS 1.0 by default. Instead of hacking user computer's registry, extend it in runtime.
             // System.Net.SecurityProtocolType lacks appropriate constants prior to .NET 4.5.
-            ServicePointManager.SecurityProtocol = (SecurityProtocolType)0x0C00;
+            ServicePointManager.SecurityProtocol = (SecurityProtocolType)0x0C00 | (SecurityProtocolType)0x3000;
+
             var xmlReader = XmlReader.Create(new MemoryStream(Encoding.UTF8.GetBytes(@"<ResourceRef Uri=""https://disco.eduvpn.org/v2/server_list.json"">
 						<MinisignPublicKeyDictionary Key=""PublicKeys"">
 							<PublicKey>RWRtBSX1alxyGX+Xn3LuZnWUT0w//B6EmTJvgaAxBMYzlQeI+jdrO6KF</PublicKey>
