faa1ea35de435d90db79d4139fc6395460674ce1
Simon Rozman <simon@rozman.si>
2022-01-11 15:07:53 +0100

    Add explicit Minisign verify test
    Signed-off-by: Simon Rozman <simon@rozman.si>

---
+++ eduVPN.Tests/Xml/MinisignVerifyTests.cs
@@ -0,0 +1,34 @@
+﻿/*
+    eduVPN - VPN for education and research
+
+    Copyright: 2017-2022 The Commons Conservancy eduVPN Programme
+    SPDX-License-Identifier: GPL-3.0+
+*/
+
+using eduVPN.Xml;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+using System.Text;
+using System.Xml;
+
+namespace eduVPN.Tests.Xml
+{
+    [TestClass]
+    public class MinisignVerifyTests
+    {
+        [TestMethod]
+        public void MinisignVerifyTest()
+        {
+            var xmlReader = XmlReader.Create(new MemoryStream(Encoding.UTF8.GetBytes(@"<ResourceRef Uri=""../../Setup/eduVPN.windows.json"">
+						<MinisignPublicKeyDictionary Key=""PublicKeys"">
+							<PublicKey>RWRPrtnepBgoU86pKtSnHJXBtmtJjv6T5wN2Q+P7vPgHPdr3v8kGme13</PublicKey>
+						</MinisignPublicKeyDictionary>
+					</ResourceRef>")));
+            while (xmlReader.ReadState == ReadState.Initial)
+                xmlReader.Read();
+            var source = new ResourceRef();
+            source.ReadXml(xmlReader);
+            eduVPN.Xml.Response.Get(source);
+        }
+    }
+}
