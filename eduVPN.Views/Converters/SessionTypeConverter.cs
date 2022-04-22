2b3fec700e329f4a12344639e3fbd336f94897bf
Simon Rozman <simon@rozman.si>
2022-04-22 17:59:39 +0200

    Display VPN protocol in Connection info GUI
    Signed-off-by: Simon Rozman <simon@rozman.si>

---
+++ eduVPN.Views/Converters/SessionTypeConverter.cs
@@ -0,0 +1,54 @@
+﻿/*
+    eduVPN - VPN for education and research
+
+    Copyright: 2017-2022 The Commons Conservancy
+    SPDX-License-Identifier: GPL-3.0+
+*/
+
+using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace eduVPN.Converters
+{
+    /// <summary>
+    /// Returns string representing session type.
+    /// </summary>
+    public class SessionTypeConverter : IValueConverter
+    {
+        #region Methods
+
+        /// <summary>
+        /// Converts a value.
+        /// </summary>
+        /// <param name="value">The value produced by the binding source</param>
+        /// <param name="targetType">The type of the binding target property</param>
+        /// <param name="parameter">The converter parameter to use</param>
+        /// <param name="culture">The culture to use in the converter</param>
+        /// <returns>A converted value. If the method returns <c>null</c>, the valid null value is used.</returns>
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return
+                value is ViewModels.VPN.OpenVPNSession ? "(OpenVPN)" :
+                value is ViewModels.VPN.WireGuardSession ? "(WireGuard)" :
+                null;
+        }
+
+        /// <summary>
+        /// Converts a value.
+        /// </summary>
+        /// <param name="value">The value that is produced by the binding target</param>
+        /// <param name="targetType">The type to convert to</param>
+        /// <param name="parameter">The converter parameter to use</param>
+        /// <param name="culture">The culture to use in the converter</param>
+        /// <returns>A converted value. If the method returns null, the valid null value is used.</returns>
+        /// <exception cref="NotImplementedException">Always</exception>
+        /// <remarks>Not implemented.</remarks>
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+}
