3fb9fc3825bba23abd2c571bfe2c2147b765e78e
Simon Rozman <simon@rozman.si>
2024-04-03 14:16:14 +0200

    Discontinue ServersNetworkTest
    Any eduVPN server down made this test fail. This is not a client
    failure. This is not even a unit test, but network test. We have
    https://status.eduvpn.org/ for this.
    
    Signed-off-by: Simon Rozman <simon@rozman.si>

--- eduVPN.Tests/Models/ServerListTests.cs
+++ eduVPN.Tests/Models/ServerListTests.cs
@@ -6,14 +6,8 @@
 */
 
 using eduVPN.JSON;
-using eduVPN.Xml;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
-using System.IO;
-using System.Net;
-using System.Text;
-using System.Threading.Tasks;
-using System.Xml;
 
 namespace eduVPN.Models.Tests
 {
@@ -48,73 +42,6 @@ namespace eduVPN.Models.Tests
 
             Assert.IsInstanceOfType(server_list_list_ia[new Uri("https://sunset.nuonet.fr/")], typeof(InstituteAccessServer));
             Assert.IsInstanceOfType(server_list_list_ia[new Uri("https://eduvpn.rash.al/")], typeof(SecureInternetServer));
-        }
-
-        [TestMethod()]
-        public void ServersNetworkTest()
-        {
-            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12 | SecurityProtocolType.Tls13;
-
-            var xmlReader = XmlReader.Create(new MemoryStream(Encoding.UTF8.GetBytes(@"<ResourceRef Uri=""https://disco.eduvpn.org/v2/server_list.json"">
-						<MinisignPublicKeyDictionary Key=""PublicKeys"">
-							<PublicKey>RWRtBSX1alxyGX+Xn3LuZnWUT0w//B6EmTJvgaAxBMYzlQeI+jdrO6KF</PublicKey>
-							<PublicKey>RWQKqtqvd0R7rUDp0rWzbtYPA3towPWcLDCl7eY9pBMMI/ohCmrS0WiM</PublicKey>
-						</MinisignPublicKeyDictionary>
-					</ResourceRef>")));
-            while (xmlReader.ReadState == ReadState.Initial)
-                xmlReader.Read();
-            var source = new ResourceRef();
-            source.ReadXml(xmlReader);
-
-            // Load list of servers.
-            var server_list_list_json = Response.Get(source);
-            var server_list_list_ia = new ServerDictionary();
-            server_list_list_ia.LoadJSON(server_list_list_json.Value);
-
-            // Load all servers APIs.
-            Parallel.ForEach(server_list_list_ia.Values, srv =>
-            {
-                var uriBuilder = new UriBuilder(srv.Base);
-                uriBuilder.Path += "info.json";
-                try
-                {
-                    new ServerEndpoints().LoadJSON(Response.Get(uriBuilder.Uri).Value);
-                }
-                catch (UnsupportedServerAPIException)
-                {
-                    // Ignore non-APIv3 servers.
-                }
-                catch (AggregateException ex)
-                {
-                    if (ex.InnerException is WebException ex_web &&
-                        (ex_web.Status == WebExceptionStatus.NameResolutionFailure || // DNS resolving failure
-                        ex_web.Status == WebExceptionStatus.ConnectFailure || // connection refused
-                        ex_web.Status == WebExceptionStatus.TrustFailure || // expired or invalid server certificate
-                        ex_web.Status == WebExceptionStatus.SecureChannelFailure || // TLS failure
-                        ex_web.Status == WebExceptionStatus.Timeout)) // server down
-                    {
-                        // Ignore connection failure WebException(s), as some servers are not publicly available or have other issues.
-                    }
-                    else
-                        throw;
-                }
-                catch (WebException ex)
-                {
-                    if (ex.Response is HttpWebResponse response &&
-                        (response.StatusCode == HttpStatusCode.NotFound || // 404 Not Found
-                        response.StatusCode == HttpStatusCode.Forbidden)) // 403 Forbidden
-                    {
-                        // Ignore specific WebException(s), as some servers have issues.
-                    }
-                    else
-                        throw;
-                }
-            });
-
-            // Re-load list of servers.
-            server_list_list_json = Response.Get(
-                res: source,
-                previous: server_list_list_json);
         }
     }
 }
